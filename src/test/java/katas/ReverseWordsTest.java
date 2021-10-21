1b3349bc1a1f8304b3a169f3d49570249caab7ec
Alejandro Rey <74797718+reyalejandro@users.noreply.github.com>
2021-10-21 12:19:40 -0300

    agregar katas alejandro rey

---
+++ src/test/java/katas/ReverseWordsTest.java
@@ -0,0 +1,35 @@
+package katas;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ReverseWordsTest {
+    // Asi tambien se puede hacer
+    /*@Test
+    public void CasosPrueba() {
+        assertEquals("aloh",ReverseWords.reverseWords("hola"));
+        assertEquals("uahc",ReverseWords.reverseWords("chau"));
+    }*/
+
+    @Test
+    public void datoEntradaRetornaStringRevesSignosOrtograficos() {
+        assertEquals(",aloh odot ?neib",ReverseWords.reverseWords("hola, todo bien?"));
+    }
+
+    @Test
+    public void datoEntradaRetornaStringReves() {
+        assertEquals("aloh",ReverseWords.reverseWords("hola"));
+    }
+
+    @Test
+    public void datoEntradaRetornaStringRevesEspacios() {
+        assertEquals("h o l a",ReverseWords.reverseWords("h o l a"));
+    }
+
+    @Test
+    public void datoEntradaRetornaStringRevesDobleEspacio() {
+        assertEquals("aloh  omoc  satse",ReverseWords.reverseWords("hola  como  estas"));
+    }
+
+}
