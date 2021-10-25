96f0005dd6f9ca706fe301a34f39bad0a8f5d136
Giuliano Ortego <Giuliano.Ortego@tsoftglobal.com>
2021-10-24 21:56:30 -0300

    Subo Katas GiulianoOrtego

---
+++ src/test/java/katas/gortego/CountingDuplicateTest.java
@@ -0,0 +1,34 @@
+package katas.gortego;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CountingDuplicateTest {
+
+    @Test
+    public void datoEntradaRetornarCero(){
+        assertEquals(0,CountingDuplicates.duplicateCount("abcde"));
+    }
+
+    @Test
+    public void datoEntradaRetornaDos(){
+        assertEquals(2, CountingDuplicates.duplicateCount("aabbcde"));
+    }
+
+    @Test
+    public void datoEntradaRetornaDosCaseSensitive(){
+        assertEquals(2, CountingDuplicates.duplicateCount("aabBcde"));
+    }
+    @Test
+    public void datoEntradaLimiteAlfabetico() {
+        assertEquals(-1, CountingDuplicates.duplicateCount("##aabb%%^^"));
+    }
+
+    @Test
+    public void datoEntradaretornaUno() {
+        assertEquals(1, CountingDuplicates.duplicateCount("iiiiii"));
+    }
+
+}
