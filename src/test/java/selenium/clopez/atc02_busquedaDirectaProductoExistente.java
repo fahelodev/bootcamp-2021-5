5937bebbeb7613fa6dffcec63dec87e411fd9a98
Christian Lopez <Christian.Lopez@tsoftglobal.com>
2021-10-25 22:14:35 -0300

    caso selenium: atc02_busquedaDirectaProductoExistente, desarrollado y terminado con exito (compilación y ejecución exitosas), se agrega al caso anterior el comando para cerrar la ventana al terminar la ejecusión

--- src/test/java/selenium/clopez/atc02_busquedaDirectaProductoExistente.java
+++ src/test/java/selenium/clopez/atc02_busquedaDirectaProductoExistente.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.interactions.Actions;
 import java.io.File;
 import java.io.IOException;
 
-public class atc01_BusquedaPalabrasClaves {
+public class atc02_busquedaDirectaProductoExistente {
     public static void main(String[] args) throws InterruptedException, IOException {
         // configuramos la vista maximizada
         FirefoxOptions options = new FirefoxOptions();
@@ -18,33 +18,32 @@ public class atc01_BusquedaPalabrasClaves {
         options.addArguments("--start-fullscreen");
         // Inicialización del WedDriver
         WebDriver driver;
-        // Caso sera realizado en firefox con xpath
+        // Caso sera realizado en firefox con css
         WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver(options);
         // 1.- Cargar Home
         driver.get("http://automationpractice.com//");
-        // espera
+        // Espera
         Thread.sleep(2000);
-        // 2.- Introducir "dress" en el campo de búsqueda
-        // se cambian las comillas dobles del cogido copiapo por comillas simples
-        // //*[@id="search_query_top"] -> //*[@id='search_query_top']
-        driver.findElement(By.xpath("//*[@id=\'search_query_top\']")).sendKeys("dress");
+        // 2.- Introducir "printed chiffon dress" en el campo de búsqueda
+        driver.findElement(By.cssSelector("#search_query_top")).sendKeys("printed chiffon dress");
         // 3.- Hacer clic en botón "Search"
-        driver.findElement(By.xpath("/html/body/div/div[1]/header/div[3]/div/div/div[2]/form/button")).click();
-        // espera
+        driver.findElement(By.cssSelector("button.btn:nth-child(5)")).click();
+        // Espera
         Thread.sleep(2000);
         // 4.- Observar los resultados de la búsqueda.
-        // bajamos un poco en la pagina
+        // Bajamos un poco en la pagina
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("javascript:window.scrollBy(250,350)");
-        WebElement element = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div[2]/ul/li[1]/div/div[1]/div/a[1]/img"));
+        // Nos posicionamos sobre el elemento buscado
+        WebElement element = driver.findElement(By.cssSelector("li.ajax_block_product:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1) > img:nth-child(1)"));
         Actions actions = new Actions(driver);
         actions.moveToElement(element);
         actions.perform();
         Thread.sleep(2000);
         // buscamos elementos señalados
         File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(screenshotFile, new File("BusquedaPalabrasClaves"+".png"));
+        FileUtils.copyFile(screenshotFile, new File("busquedaDirectaProductoExistente"+".png"));
         // Esperamos
         Thread.sleep(2000);
         // Cerramos la ventana
