a7aa61d3ee53257e5a016cdea7396731fe9de80e
Christian Lopez <Christian.Lopez@tsoftglobal.com>
2021-10-26 17:36:01 -0300

    ejercicios realizados anteriormente se cambian a versiones beta y se deja en comentario la captura de pantalla junto a la eliminación de imagenes ... se crea la version tes del primer ejercicio, ejecutada satisfactoriamente

--- src/test/java/selenium/clopez/atc01_BusquedaPalabrasClaves.java
+++ src/test/java/selenium/clopez/atc01_BusquedaPalabrasClaves.java
@@ -1,53 +1,67 @@
 package selenium.clopez;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.*;
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.interactions.Actions;
 
-import java.io.File;
-import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 
 public class atc01_BusquedaPalabrasClaves {
-    public static void main(String[] args) throws InterruptedException, IOException {
-        // configuramos la vista maximizada
-        FirefoxOptions options = new FirefoxOptions();
-        options.addArguments("--start-maximized");
-        options.addArguments("--start-fullscreen");
-        // Inicialización del WedDriver
-        WebDriver driver;
-        // Caso sera realizado en firefox con xpath
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void Setup(){
+        System.out.println("Setup necesario antes de Instanciar");
         WebDriverManager.firefoxdriver().setup();
-        driver = new FirefoxDriver(options);
-        // 1.- Cargar Home
+
+    }
+    @Before
+    public void init() throws InterruptedException {
+        System.out.println("init para instanciar");
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
         driver.get("http://automationpractice.com//");
-        // espera
-        Thread.sleep(2000);
-        // 2.- Introducir "dress" en el campo de búsqueda
-        // se cambian las comillas dobles del cogido copiapo por comillas simples
-        // //*[@id="search_query_top"] -> //*[@id='search_query_top']
-        driver.findElement(By.xpath("//*[@id=\'search_query_top\']")).sendKeys("dress");
-        // 3.- Hacer clic en botón "Search"
-        driver.findElement(By.xpath("/html/body/div/div[1]/header/div[3]/div/div/div[2]/form/button")).click();
-        // espera
-        Thread.sleep(2000);
-        // 4.- Observar los resultados de la búsqueda.
-        // bajamos un poco en la pagina
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("javascript:window.scrollBy(250,350)");
-        WebElement element = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div[2]/ul/li[1]/div/div[1]/div/a[1]/img"));
-        Actions actions = new Actions(driver);
-        actions.moveToElement(element);
-        actions.perform();
-        Thread.sleep(2000);
-        // buscamos elementos señalados
-        File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(screenshotFile, new File("BusquedaPalabrasClaves"+".png"));
         // Esperamos
         Thread.sleep(2000);
-        // Cerramos la ventana
-        driver.close();
+        while (driver.getTitle() == "508 Error - Resource Limit Reached"){
+            driver.navigate().refresh();
+            // Esperamos
+            Thread.sleep(1000);
+        }
+    }
+
+    @Test
+    public void atc01_BusquedaPalabrasClaves() throws InterruptedException {
+        driver.findElement(By.xpath("//*[@id=\'search_query_top\']")).sendKeys("dress");
+        driver.findElement(By.xpath("/html/body/div/div[1]/header/div[3]/div/div/div[2]/form/button")).click();
+        // Esperamos
+        Thread.sleep(2000);
+        // se crea una lista de elementos web
+        // List<WebElement> dress = driver.findElements(By.xpath("//*[@id=\"center_column\"]/ul/li"));
+        List<WebElement> dress = driver.findElements(By.xpath("/html/body/div/div[2]/div/div[3]/div[2]/ul/li"));
+        // Tamaño obtenido = 7 en ambas listas anteriores
+        // System.out.println("Tamaño = "+dress.size());
+        Assert.assertTrue(dress.size() > 2);
+    }
+
+    @After
+    public void close(){
+        // Espera
+        if(driver != null){
+            driver.close();
+        }
+
+    }
+
+    @AfterClass
+    public static void closeAll(){
+        System.out.println("closeAll :: Cerrar otras conexiones que fueron utilizadas en el test");
+
     }
 }
--- src/test/java/selenium/clopez/atc01_BusquedaPalabrasClaves_Beta.java
+++ src/test/java/selenium/clopez/atc01_BusquedaPalabrasClaves_Beta.java
@@ -1,16 +1,17 @@
 package selenium.clopez;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.interactions.Actions;
 
-import java.io.File;
 import java.io.IOException;
 
-public class atc02_busquedaDirectaProductoExistente {
+public class atc01_BusquedaPalabrasClaves_Beta {
     public static void main(String[] args) throws InterruptedException, IOException {
         // configuramos la vista maximizada
         FirefoxOptions options = new FirefoxOptions();
@@ -18,32 +19,33 @@ public class atc02_busquedaDirectaProductoExistente {
         options.addArguments("--start-fullscreen");
         // Inicialización del WedDriver
         WebDriver driver;
-        // Caso sera realizado en firefox con css
+        // Caso sera realizado en firefox con xpath
         WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver(options);
         // 1.- Cargar Home
         driver.get("http://automationpractice.com//");
-        // Espera
+        // espera
         Thread.sleep(2000);
-        // 2.- Introducir "printed chiffon dress" en el campo de búsqueda
-        driver.findElement(By.cssSelector("#search_query_top")).sendKeys("printed chiffon dress");
+        // 2.- Introducir "dress" en el campo de búsqueda
+        // se cambian las comillas dobles del cogido copiapo por comillas simples
+        // //*[@id="search_query_top"] -> //*[@id='search_query_top']
+        driver.findElement(By.xpath("//*[@id=\'search_query_top\']")).sendKeys("dress");
         // 3.- Hacer clic en botón "Search"
-        driver.findElement(By.cssSelector("button.btn:nth-child(5)")).click();
-        // Espera
+        driver.findElement(By.xpath("/html/body/div/div[1]/header/div[3]/div/div/div[2]/form/button")).click();
+        // espera
         Thread.sleep(2000);
         // 4.- Observar los resultados de la búsqueda.
-        // Bajamos un poco en la pagina
+        // bajamos un poco en la pagina
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("javascript:window.scrollBy(250,350)");
-        // Nos posicionamos sobre el elemento buscado
-        WebElement element = driver.findElement(By.cssSelector("li.ajax_block_product:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1) > img:nth-child(1)"));
+        WebElement element = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div[2]/ul/li[1]/div/div[1]/div/a[1]/img"));
         Actions actions = new Actions(driver);
         actions.moveToElement(element);
         actions.perform();
         Thread.sleep(2000);
         // buscamos elementos señalados
-        File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(screenshotFile, new File("busquedaDirectaProductoExistente"+".png"));
+        // File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        // FileUtils.copyFile(screenshotFile, new File("BusquedaPalabrasClaves"+".png"));
         // Esperamos
         Thread.sleep(2000);
         // Cerramos la ventana
