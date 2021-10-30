66294626da7528c8ef6cf1740572ea0bfe9b4ae3
Carlos Andres Luna Salas <Carlos.Luna@tsoftglobal.com>
2021-10-29 21:50:20 -0300

    Se sube la actividad de babtuto

--- src/main/java/katas/landres/ZooDingleMouse.java
+++ src/main/java/katas/landres/ZooDingleMouse.java
@@ -1,2 +1,20 @@
-package katas.landres;public class ZooDingleMouse {
+/*package katas.landres;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+
+
+public class ZooDingleMouse {
+
+    public  String [] whoEatsWho(final String zoo){
+
+        String []animalsAll=zoo.split(",");  //utiliza  un arreglo de string para guardar la lista de animales
+        List<String> listP= new ArrayList<String>(); //lista para poder pushear los animales
+        String animalDeath;
+        String []
+
 }
+/*
