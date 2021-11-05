55749a00f55ed093aeedb627ec979d644d675159
francojcuevas <franco.j.cuevas@gmail.com>
2021-11-05 04:35:39 -0300

    Agrego los casos de vuelos y automoviles automatizados falabella

--- src/test/java/viajesfalabella/equipo6/AutomationVuelosFalabella.java
+++ src/test/java/viajesfalabella/equipo6/AutomationVuelosFalabella.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
+//
 import static org.junit.Assert.assertEquals;
 
 public class AutomationVuelosFalabella {
