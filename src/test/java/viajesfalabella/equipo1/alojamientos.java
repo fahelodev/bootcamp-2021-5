cf4cc205be4c2a93a82f61389026b280a5eb08f6
Ricardo Olguin <Ricardo.Olguin@tsoftglobal.com>
2021-11-11 13:31:23 -0300

    Refactorizacion alojamientos completada

--- src/test/java/viajesfalabella/equipo1/alojamientos.java
+++ src/test/java/viajesfalabella/equipo1/alojamientos.java
@@ -116,10 +116,10 @@ public class alojamientos {
             }
         }
     }
-
+////select[@class='select-tag']/option[@value='total_price_ascending']
     private void filtrar (String filtro){
         WebDriverWait espera = new WebDriverWait(driver, 15);
-        List<WebElement> optionsTO = driver.findElements(By.cssSelector("aloha-select.sorting-select div.eva-3-select.-sm div.select-container select option"));
+        List<WebElement> optionsTO = driver.findElements(By.xpath("//aloha-select[@class='sorting-select']//select[@class='select-tag']/option"));
         System.out.println(optionsTO);
 
         espera.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".eva-3-select.-sm .select-tag")));
@@ -134,7 +134,7 @@ public class alojamientos {
     }
 
     private String patron(String comparar, String selector){
-        WebElement ubicacion = driver.findElement(By.cssSelector(selector));
+        WebElement ubicacion = driver.findElement(By.xpath(selector));
         String nombre = ubicacion.getText();
 
         Pattern patron = Pattern.compile(comparar);
@@ -158,7 +158,6 @@ public class alojamientos {
         espera.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div.results-cluster-container")));
         int lista = driver.findElements(By.cssSelector("div.results-cluster-container")).size();
 
-
         Assert.assertTrue(lista > 1);
     }
 
@@ -172,7 +171,7 @@ public class alojamientos {
         seleccionarDestino("dub","Dubái, Dubai, Emiratos Árabes Unidos");
 
         espera.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.sbox-dates-input.sbox-datein-container > div > input")));
-        driver.findElement(By.cssSelector("div.sbox-dates-input.sbox-datein-container > div > input")).click();
+        driver.findElement(By.xpath("//input[@placeholder='Entrada']")).click();
 
         //SELECCIONA LA FECHAS
         calendario(1,7);
@@ -184,14 +183,17 @@ public class alojamientos {
         seleccionarAdultos(1);
         driver.findElement(By.cssSelector("div.sbox-button")).click();
 
-        espera.until(ExpectedConditions.elementToBeClickable(By.cssSelector("aloha-select.sorting-select div.eva-3-select.-sm div.select-container select")));
-        driver.findElement(By.cssSelector("aloha-select.sorting-select div.eva-3-select.-sm div.select-container select")).click();
+        espera.until(ExpectedConditions.elementToBeClickable(By.xpath("//aloha-select[@class='sorting-select']//select[@class='select-tag']")));
+        driver.findElement(By.xpath("//aloha-select[@class='sorting-select']//select[@class='select-tag']")).click();
         //SE APLICA EL FILTRO DESEADO
         filtrar("Precio: menor a mayor");
         //UTILIZCION DE EXPRECION REGULAR PARA COMPARAR PALABRAS
         espera.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button.eva-3-btn.-md.-primary.-eva-3-fwidth")));
-        String res = patron("Dubai","aloha-cluster-accommodation-info-container > aloha-location-name > span");
-        String res2 = patron("6","div.pricebox-value-container > aloha-price-container > aloha-summary-price > p.eva-3-p.-eva-3-tc-gray-2.first-message");
+
+        ////div[@class='cluster-description-wrapper -eva-3-fwidth']//span[@class='-eva-3-tc-gray-2']
+        String res = patron("Dubái","//span[@class='-eva-3-tc-gray-2']");
+        String res2 = patron("6","//p[@class='eva-3-p -eva-3-tc-gray-2 first-message']");
+
 
         assertEquals("true", res);
         assertEquals("true", res2);
@@ -225,11 +227,11 @@ public class alojamientos {
 
         //filtrar por reserva flexible
         espera.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.eva-3-filters-vertical.-eva-3-mb-lg.-show-filters")));
-        driver.findElement(By.cssSelector(".eva-3-dropdown .dropdown-item.-active .dropdown-subitem:first-of-type .dropdown-item-container")).click();
+        driver.findElement(By.xpath("//li[contains(.,'Reserva flexible')]//em")).click();
 
-        //filtrar por reserva departamento
+
         espera.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.eva-3-filters-vertical.-eva-3-mb-lg.-show-filters")));
-        List<WebElement> optionsTO = driver.findElements(By.cssSelector("div.eva-3-filters-vertical.-eva-3-mb-lg.-show-filters ul.eva-3-dropdown li.dropdown-item.-active aloha-checkbox-filter ul li.dropdown-subitem"));
+        List<WebElement> optionsTO = driver.findElements(By.xpath("//li[contains(.,'Tipo de Alojamiento')]//em"));
         System.out.println(optionsTO);
         for (WebElement dep : optionsTO) {
             if (dep.getText().contains("Departamentos")) {
@@ -237,10 +239,10 @@ public class alojamientos {
                 break;
             }
         }
-
-        espera.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.eva-3-pricebox-cluster.-responsive div.pricebox-top-container div.pricebox-action button")));
+//
+        espera.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='pricebox-action']")));
         Thread.sleep(7000);
-        driver.findElement(By.cssSelector("div.cluster-pricebox-container div.eva-3-pricebox-cluster.-responsive div.pricebox-top-container div.pricebox-action button")).click();
+        driver.findElement(By.xpath("//button[@class='eva-3-btn -md -primary -eva-3-fwidth']")).click();
 
         //Metodo para cambiar de pestaña
         String mainTab = driver.getWindowHandle();
@@ -258,18 +260,19 @@ public class alojamientos {
 
 
         Thread.sleep(2000);
-        driver.findElement(By.cssSelector("body > aloha-app-root > aloha-detail > div > div:nth-child(4) > div > div > aloha-infobox-container > aloha-infobox-wrapper-container > div > div > div > aloha-infobox-shopping-content > div > div.pricebox-action.-eva-3-mt-lg.pricebox-button > aloha-button > button")).click();
+        driver.findElement(By.xpath("//div[@class='pricebox-top-container']//button")).click();
 
         Thread.sleep(2000);
-        driver.findElement(By.cssSelector("#roompacks-container-wrapper > aloha-roompacks-container > aloha-roompacks-grid-container > div.body > div.col-6.-eva-3-bc-white > aloha-reservation-summary-container > div > aloha-next-step-button > aloha-button > button")).click();
+        driver.findElement(By.xpath("//div[@class='-eva-3-mb-lg']//button")).click();
 
         Thread.sleep(2000);
-        driver.findElement(By.cssSelector("#pricebox-overlay > div.eva-3-pricebox-sticky.eva-3-shadow.price-box-sticky.price-box-shadow.-loyalty.-with-info > div > button > em")).click();
+        driver.findElement(By.xpath("//div[@class='pricebox-content-container']//button")).click();
 
         Thread.sleep(5000);
 
-        espera.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.eva-3-message.-success.-eva-3-mb-md.-eva-3-mt-md div.message-header span")));
-        String res = patron("¡Felicitaciones! Seleccionaste la habitación más económica","div.eva-3-message.-success.-eva-3-mb-md.-eva-3-mt-md div.message-header span");
+        espera.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='eva-3-message -success -eva-3-mb-md -eva-3-mt-md']//span")));
+        Thread.sleep(5000);
+        String res = patron("¡Felicitaciones! Seleccionaste la habitación más económica","//div[@class='eva-3-message -success -eva-3-mb-md -eva-3-mt-md']//span");
 
         assertEquals("true",res);
 
@@ -282,7 +285,7 @@ public class alojamientos {
     @After
     public void close() {
         if (driver != null) {
-            driver.close();
+           driver.close();
         }
     }
 
