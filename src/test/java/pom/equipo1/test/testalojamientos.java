101278711c86ecd5b94f33024d94aaf377de6050
Ricardo Olguin <Ricardo.Olguin@tsoftglobal.com>
2021-11-12 17:50:58 -0300

    agrego lo realizado en testalojamientos

--- src/test/java/pom/equipo1/test/testalojamientos.java
+++ src/test/java/pom/equipo1/test/testalojamientos.java
@@ -2,46 +2,46 @@ package pom.equipo1.test;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import pom.equipo1.base.TestBase;
 import pom.equipo1.pages.VFHomePage;
+import pom.equipo1.pages.VFalojamientosPage;
 import pom.equipo1.pages.VFpaquetesPage;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class testpaquetes extends TestBase {
+public class testalojamientos extends TestBase {
 
     protected VFHomePage paginaHome = null;
-    protected VFpaquetesPage paginaPaquetes = null;
+    protected VFalojamientosPage paginaAlojamientos = null;
 
     @Before
     public void init(){
         paginaHome = new VFHomePage(driver);
         paginaHome.irHomePage();
-        paginaPaquetes = new VFpaquetesPage(driver);
-        paginaPaquetes.irPaquetesDesdeHome();
+        paginaAlojamientos = new VFalojamientosPage(driver);
+        paginaAlojamientos.irAlojamientosDesdeHome();
     }
 
     @Test
-    public void CdP03_busquedaPaquetes(){
-        paginaPaquetes.seleccionarVuelo1Alojamiento();
-        paginaPaquetes.llenarCasillaOrigen("santiago","Santiago de Chile");
-        paginaPaquetes.llenarCasillaDestino("san pedro","San Pedro de Atacama");
-        paginaPaquetes.seleccionarFechas(1,2);
-        paginaPaquetes.seleccionarCantidadPasajeros(2);
-        paginaPaquetes.darClickBuscar();
-        int lista = paginaPaquetes.resultadosBusqueda();
-        assertTrue(lista>1);
+    public void CdP09_busquedaAlojamiento(){
+        paginaAlojamientos.seleccionarNoHedecidoFecha();
+        ////se cae sin una espera(ver)
+        paginaAlojamientos.llenarCasillaDestino("orlando","Orlando, Florida, Estados Unidos");
+        paginaAlojamientos.darClickBotonBuscar();
+        int cantidadResultados=paginaAlojamientos.resultadosBusquedaAlojamientos();
+        assertTrue(cantidadResultados>1);
     }
 
     @Test
-    public void CdP02_busquedaPaquetes(){
-        paginaPaquetes.llenarCasillaOrigen("santiago","Santiago de Chile");
-        paginaPaquetes.llenarCasillaDestino("buenos aires","Ciudad de Buenos Aires");
-        paginaPaquetes.seleccionarFechas(1,2);
-        paginaPaquetes.darClickBuscar();
-        String validar = paginaPaquetes.validarLugarHotel("Buenos Aires");
-        assertEquals("true",validar);
+    public void CdP08_busquedaAlojamiento(){
+        paginaAlojamientos.llenarCasillaDestino("dub","Dubái, Dubai, Emiratos Árabes Unidos");
+        //se cae sin una espera(ver)
+        paginaAlojamientos.seleccionarFechas(1,7);
+        paginaAlojamientos.seleccionarCantidadOcupantesHabitacion(1);
+        paginaAlojamientos.darClickBotonBuscar();
+
+
     }
+
 }
