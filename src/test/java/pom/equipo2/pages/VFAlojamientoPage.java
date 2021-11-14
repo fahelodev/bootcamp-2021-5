9564b1c975a158cf02f939d6a8204b5d326a73ed
Juan José Vega <Juan.Vega@tsoftglobal.com>
2021-11-14 20:22:24 -0300

    Agrego BusquedaAlojamientoPage y refactorizacion de test y pagina Alojamiento

--- src/test/java/pom/equipo2/pages/VFAlojamientoPage.java
+++ src/test/java/pom/equipo2/pages/VFAlojamientoPage.java
@@ -2,8 +2,9 @@ package pom.equipo2.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import pom.equipo2.base.SeleniumBase;
+
+import static org.junit.Assert.assertEquals;
 
 public class VFAlojamientoPage extends SeleniumBase {
     public VFAlojamientoPage(WebDriver driver) {
@@ -12,7 +13,6 @@ public class VFAlojamientoPage extends SeleniumBase {
 
     //atributos - objeto a guardar
     By btnAlojamiento = By.xpath("//label[contains(text(),'Alojamientos')]");
-    By desplegableHabitaciones = By.cssSelector("div.sbox-distri-container");
     By btnHabitaciones = By.cssSelector("div.sbox-distri-container");
     By btnMasHabitaciones = By.cssSelector("div._pnlpk-itemRow__item._pnlpk-stepper-minors.-medium-down-to-lg  a.steppers-icon-right.sbox-3-icon-plus");
     By btnAplicar = By.xpath("//a[contains(text(),'Aplicar')]");
@@ -21,15 +21,12 @@ public class VFAlojamientoPage extends SeleniumBase {
     By btnBuscar = By.xpath("//em[contains(text(),'Buscar')]");
     By listaElementos = By.cssSelector("div.ac-container ul>li");
     By listaFechasMes= By.xpath("//span[@class='_dpmg2--date-number']");
-    By etiqueteCentro= By.xpath("//span[contains(text(),'Centro') and not(contains(text(),'de'))]/ancestor::em");
-    By listaMoneda = By.cssSelector("Select.select-tag");
-    By campoMinimo = By.xpath("//input[@class='input-tag' and @placeholder=@min]");
-    By campoMaximo = By.xpath("//input[@class='input-tag' and @placeholder=@max]");
-    By btnAplicarMoneda = By.xpath("//em[contains(text(),'Aplicar')]");
+    By mensajeError = By.xpath("//p[contains(text(),'Ingresa la edad del menor')]");
+    String resultado;
 
     public void irAlojamientoDesdeHome(){
         clickear(btnAlojamiento);
-        esperaExplicitaElementoClickeable(desplegableHabitaciones,7);
+       // esperaExplicitaElementoClickeable(desplegableHabitaciones,7);
     }
     public void agregarMenorHabitacion(){
 
@@ -37,7 +34,6 @@ public class VFAlojamientoPage extends SeleniumBase {
         clickear(btnMasHabitaciones);
     }
     public void confirmarSeleccionHabitacionMenor(){
-
         clickear(btnAplicar);
     }
     public void cargarDestino(){
@@ -52,15 +48,12 @@ public class VFAlojamientoPage extends SeleniumBase {
     public void confirmarBusqueda(){
         clickear(btnBuscar);
     }
-    public void confirmarCentro(){
-        clickear(etiqueteCentro);
-    }
-    public void confirmarMoneda (){
-        seleccionarMoneda(listaMoneda);
-        esperaExplicitaPrecenciaElementos(campoMinimo,7);
-        ingresarValorMinimo(campoMinimo,"110");
-        ingresarValorMaximo(campoMaximo,"400");
-        clickear(btnAplicarMoneda);
 
+
+    public void verificarBusquedaSinEdad(){
+         resultado = obtenerTexto(mensajeError);
+        assertEquals("Ingresa la edad del menor", resultado);
     }
+
+
 }
