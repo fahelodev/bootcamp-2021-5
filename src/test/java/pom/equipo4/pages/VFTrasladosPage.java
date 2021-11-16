38129f397595d1bcc53e686a36bef5ebc39fea52
Giuliano Ortego <Giuliano.Ortego@tsoftglobal.com>
2021-11-16 17:46:56 -0300

    modificando carpetas

--- src/test/java/pom/equipo4/pages/VFTrasladosPage.java
+++
@@ -1,8 +0,0 @@
-package pom.equipo4.pages;
-
-import org.openqa.selenium.WebDriver;
-
-public class VFTrasladosPage extends SeleniumBase {
-    public VFTrasladosPage(WebDriver driver) { super(driver); }
-
-}
